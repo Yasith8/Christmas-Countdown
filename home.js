22c4b5f40343f6d3beeb6aec9691c75db8334e27
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-20 14:22:50 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,25 +1,45 @@
 const days = document.getElementById("days");
+
 const hours = document.getElementById("hours");
+
 const minutes = document.getElementById("minutes");
+
 const seconds = document.getElementById("seconds");
+
+
 
 const currentYear = new Date().getFullYear();
 
 const cristmasDay = new Date(`December 25 ${currentYear} 00:00:00`);
 
+
+
 function updateCountdowntime() {
+    
     const currentTime = new Date();
+    
     const diff = cristmasDay - currentTime;
 
+    
+    
     const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    
     const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    
     const m = Math.floor(diff / 1000 / 60) % 60;
+    
     const s = Math.floor(diff / 1000) % 60;
 
+    
+    
     days.innerHTML = d;
+    
     hours.innerHTML = h < 10 ? '0' + h : h;
+   
     minutes.innerHTML = m < 10 ? '0' + m : m;
+    
     seconds.innerHTML = s < 10 ? '0' + s : s;
 }
+
 
 setInterval(updateCountdowntime, 1000);
